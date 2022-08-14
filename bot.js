c98adb869ba7c547475f5227115c3e885bdae154
zouhir99 <109313085+zouhir99@users.noreply.github.com>
2022-08-14 05:44:53 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,10 +33,10 @@ global.APIKeys = {
 }
 
 //settings
-global.owner = ['27634090203',''] //owner number +27634090203
-global.wame = ['27634090203'] 
-global.pemilik = ['27634090203'] //owner number +27634090203
-global.premium = ['27634090203'] //premium number +27634090203
+global.owner = ['212614613169',''] //owner number +27634090203
+global.wame = ['212614613169'] 
+global.pemilik = ['212614613169'] //owner number +27634090203
+global.premium = ['212614613169'] //premium number +27634090203
 global.pengguna = 'Drips' //username
 global.footer = 'ZIM BOT INC'
 global.botnma = 'ZIM BOT INC' //bot name
